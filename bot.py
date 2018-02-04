4b9df4f5ef332a2747ec24d031d801f26174092c
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-02-04 09:11:32 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,3 +15,17 @@ from contextlib import redirect_stdout
 from discord.ext import commands
 import json
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('?'),description="Specialized bot made for HWCWL.\n\nHelp Commands",owner_id=277981712989028353)
+
+
+def cleanup_code(content):
+    # remove ```py\n```
+    if content.startswith('```') and content.endswith('```'):
+        return '\n'.join(content.split('\n')[1:-1])
+
+    return content.strip('` \n')
+    
+  
+@bot.event
+async def on_ready():
+    print('Bot is online, and ready to ROLL!')
+    await bot.change_presence(game=discord.Game(name="with HWCWL!"))
