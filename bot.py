b718c6c29bfe5e683327e1eeac2b125b7652a8fc
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-02-04 09:09:00 -0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+import os
+import io
+import traceback
+import sys
+import time
+import datetime
+import asyncio
+import random
+import aiohttp
+import pip
+import random
+import textwrap
+from contextlib import redirect_stdout
+from discord.ext import commands
+import json
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('?'),description="Specialized bot made for HWCWL.\n\nHelp Commands",owner_id=277981712989028353)
